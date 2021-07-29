b04c3208bda7e0b0a8b6ad231b61f7831e5662de
Pritam Pal <pritampal909@gmail.com>
2021-07-29 12:48:50 +0530

    All routs are added

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+
+
+
+// Get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -30,6 +34,7 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+// Adding a new tour
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTours = Object.assign({ id: newId }, req.body);
@@ -41,6 +46,56 @@ app.post('/api/v1/tours', (req, res) => {
         tours: newTours 
       }
     })
+  })
+})
+
+// Gat parameters from the url and use them
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params)
+
+  const tour = tours.find(el => el.id === Number(req.params.id))
+
+  if(tours.length < req.params.id) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+})
+
+// patch request
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if(Number(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour is hear>'
+    }
+  });
+})
+
+// Delete
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if(Number(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
   })
 })
 
