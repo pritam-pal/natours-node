575c0a05967472b5f46443de8ca591425cb80b33
Pritam Pal <pritampal909@gmail.com>
2021-08-13 20:45:06 +0530

    Param middleWare and chaining meddleWares

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,77 +1,85 @@
 const fs = require('fs');
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkId = (req, res, next, val) => {
+  if (tours.length < req.params.id) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'name or price is messing',
+    });
+  }
+  next();
+};
 
 // Tour Routs
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime)
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedTime: req.requestTime,
     result: tours.length,
     data: {
-      tours
-    }
-  })
-}
+      tours,
+    },
+  });
+};
 
 exports.addNewTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTours = Object.assign({ id: newId }, req.body);
-  tours.push(newTours)
-  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tours: newTours 
-      }
-    })
-  })
-}
+  tours.push(newTours);
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTours,
+        },
+      });
+    }
+  );
+};
 
-exports.getTourById =  (req, res) => {
-  console.log(req.params)
+exports.getTourById = (req, res) => {
+  console.log(req.params);
 
-  const tour = tours.find(el => el.id === Number(req.params.id))
+  const tour = tours.find((el) => el.id === Number(req.params.id));
 
-  if(tours.length < req.params.id) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }
   res.status(200).json({
     status: 'success',
     data: {
-      tour
-    }
-  })
-}
+      tour,
+    },
+  });
+};
 
 exports.updateTour = (req, res) => {
-  if(Number(req.params.id) > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour is hear>'
-    }
+      tour: '<Updated tour is hear>',
+    },
   });
-}
+};
 
-exports.deleteTour =  (req, res) => {
-  if(Number(req.params.id) > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
+exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: null
-  })
-}
+    data: null,
+  });
+};
